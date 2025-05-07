f47851950783ce538c1012a55d4ed8d2e341b439
Njord <Njord@TARDIS>
2025-05-07 10:54:50 +0200

    Login page final changes

--- Bookings_WinUI/Pages/LogInPage.xaml.cs
+++ Bookings_WinUI/Pages/LogInPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using ABI.Windows.UI;
+using Microsoft.UI.System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +29,16 @@ namespace Bookings_WinUI.Pages
         public LogInPage()
         {
             this.InitializeComponent();
+        }
+
+        public void ShowErrors_BtnClick(object sender, RoutedEventArgs e)
+        {
+            myStoryboard.Begin();
+            //Show errors
+            UsernameInput.BorderBrush = Application.Current.Resources["SystemFillColorAttentionBrush"] as SolidColorBrush;
+            usernameError.Visibility = Visibility.Visible;
+            PasswordInput.BorderBrush = Application.Current.Resources["SystemFillColorAttentionBrush"] as SolidColorBrush;
+            passwordError.Visibility = Visibility.Visible;
         }
     }
 }
